29adb4071d741b1f368d4a05c9b9892e1a4e7acf
Tim Lukas <lukastim@naf-cms12.desy.de>
2023-02-28 10:25:29 +0100

    added pngs to ignore

--- bachelorarbeit/IPython.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import numpy as np
-
-path = Path('/nfs/dust/cms/user/kramerto/hbt_resonant_run2/HHSkims/SKIMS_uhh_2017_v2_31Aug22/SKIM_GluGluHToTauTau/output0.npz')
-import IPython 
-IPython.embed()
