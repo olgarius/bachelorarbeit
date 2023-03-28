1901e28d8c34dad11bc75596d66263f5c924c4b0
Tim Lukas <lukastim@naf-cms12.desy.de>
2023-03-28 12:50:56 +0200

    Improved Testing

--- bachelorarbeit/tests/test.py
+++ bachelorarbeit/tests/test.py
@@ -1,6 +1,6 @@
 import numpy as np
-import bachelorarbeit.grid as grid
-import bachelorarbeit.mathutil as u
+import grid as grid
+import mathutil as u
 import plotting
 
 import multiprocessing as mp
@@ -11,24 +11,26 @@ def testGrid():
         return 1000*x*x
 
     def f2(x):
-        return 0
+        return x**2
 
     def f3(x,y):
-        return 0
+        return x*y
 
     g = grid.grid()
 
-    x = np.arange(0,6)
-    y = np.arange(-5,0)
-
+    x = np.arange(0,6,0.1)
+    y = np.arange(-5,0,0.01)
+    z = np.arange(1,3,0.02)
+    print(len(x),len(y),len(z))
 
     g.addDimension(x)
     g.addDimension(y)
-    
+    g.addDimension(z)
 
     g.addFunction(f1,[1])
     g.addFunction(f2,[2])
     g.addFunction(f3,[1,2])
+    g.addFunction(f3,[2,3])
 
     g.evaluate()
 
@@ -39,7 +41,7 @@ def testGrid():
     print(e)
     print(m)
     print(p)
-    plotting.plot2d3(e,g.evaValues,'bachelorarbeit/','x','y',[-6,6],[-6,6])
+    plotting.plot2d3(np.amin(e,2),g.evaValues,'bachelorarbeit/','x','y',[-6,6],[-6,6])
 
 def testUtil():
     p1 = (1,np.pi,1)
